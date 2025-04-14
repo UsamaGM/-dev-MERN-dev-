5fbb15ffd46f26da91acfa0e60cbb08084220722
KrishnaParhyar <parhyarkrishna@gmail.com>
2025-04-14 14:55:42 +0500

    ProgressBar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Header from './component/Header'
 import Courses from './component/Courses'
+import ProgressBar from './component/ProgressBar'
+import Login from './component/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
     <>
       <Header/>
       <Courses/>
+      <ProgressBar />
+      <Login />
     </>
   )
 }
