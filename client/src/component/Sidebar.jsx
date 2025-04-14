6b65c1eb4876dbd6eb950c182bd03cb8bf2b886b
salwaaliakbar <salwaaliakbar22@gmail.com>
2025-04-14 16:34:55 +0500

    resolve conflict of sidebar

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logout from '../assets/images/logout.png'
 import profile from '../assets/images/person.png'
 import home from '../assets/images/home.png'
@@ -46,43 +45,3 @@ function SideBar() {
     )
 }
 export default SideBar
-=======
-import React from "react";
-import img from "../assets/images/blackboard-class.svg";
-
-const Sidebar = () => {
-  return (
-    <div className="bg-sky-700 px-6 py-2 text-white text-xl rounded-3xl w-40 flex justify-center items-center cursor-pointer hover:bg-sky-600 mr-6">
-                  <img className="p-2" src={img} alt="Search Icon" />
-                  <p> username</p>
-    </div>
-
-
-
-    // <div className="w-52 h-screen bg-white border-r p-5 space-y-6">
-    //   <div className="flex items-center space-x-2 cursor-pointer">
-    //     <span className="text-xl">⏱️</span>
-    //     <span className="text-base">Dashboard</span>
-    //   </div>
-    //   <div className="flex items-center space-x-2 cursor-pointer">
-    //     <span className="text-xl">🏬</span>
-    //     <span className="text-base">Restautant’s</span>
-    //   </div>
-    //   <div className="flex items-center space-x-2 cursor-pointer">
-    //     <span className="text-xl">👤</span>
-    //     <span className="text-base">Manage User’s</span>
-    //   </div>
-    //   <div className="flex items-center space-x-2 cursor-pointer">
-    //     <span className="text-xl">📦</span>
-    //     <span className="text-base">Manage Order’s</span>
-    //   </div>
-    //   <div className="flex items-center space-x-2 cursor-pointer">
-    //     <span className="text-xl">🏷️</span>
-    //     <span className="text-base">Manage Coupon’s</span>
-    //   </div>
-    // </div>
-  );
-};
-
-export default Sidebar;
->>>>>>> 4cbcadb8da0249fceff0274d193dcda04e5f9be1
