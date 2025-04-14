bfb3c30b00663dc754cee8e447625e7209552ce2
UsamaGM <usamapk7861@gmail.com>
2025-04-14 15:55:19 +0500

    add add classRoute classController

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./routes/userRoute.js";
+import classRouter from "./routes/classRoute.js";
 import cors from "cors";
 
 const app = express();
@@ -12,6 +13,7 @@ app.get("/test", async function (req, res) {
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
+app.use("/class", classRouter);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
