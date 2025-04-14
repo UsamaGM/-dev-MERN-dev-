d6c74216a98bd57c2aec1415820e99359a7007cf
UsamaGM <usamapk7861@gmail.com>
2025-04-14 16:43:36 +0500

    add authentication, authorization, email service

---
+++ server/services/emailService.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+async function sendEmail(to, subject, html) {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Email successfully sent to:", to);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error; // Re-throw the error to be handled by the caller
+  }
+}
+
+export { sendEmail };
