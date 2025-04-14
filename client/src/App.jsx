0deb2a3a721497d8b07f3b24a75f1ef86237cad0
salwaaliakbar <salwaaliakbar22@gmail.com>
2025-04-14 16:32:56 +0500

    add sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './component/Header'
 import Courses from './component/Courses'
 import Card from './component/Card'
 import ProgressBar from './component/ProgressBar'
+import SideBar from './component/Sidebar'
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Header/>  
       <Card/>
       <Courses/>
-     
       <ProgressBar />
+      <SideBar/>
     </>
   )
 }
