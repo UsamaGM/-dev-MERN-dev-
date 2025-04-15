ac12341c5f448fca105b14d1eff2a1bf4b7a2b3e
salwaaliakbar <salwaaliakbar22@gmail.com>
2025-04-15 10:57:50 +0500

    set errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,23 @@ import Grades from './component/Grades'
 import { Routes, Route } from 'react-router-dom';
 import Login from './component/Login';
 import SignUp from './component/SignUp';
+import Profile from './component/Profile';
 
 function App() {
   return (
     <>
       <Routes>
-      <Route path='/' element={<Login />}></Route>
+        <Route path='/' element={<Login />}></Route>
         <Route path='/studentDashBoard' element={<StudentDashBoard />}></Route>
-        <Route path='/myCourses' element={<MyCourses/>}></Route>
-        <Route path='/quizzes' element={<Quizzes/>}></Route>
-        <Route path='/presentation' element={<Presentation/>}></Route>
-        <Route path='/grades' element={<Grades/>}></Route>
+        <Route path='/myCourses' element={<MyCourses />}></Route>
+        <Route path='/quizzes' element={<Quizzes />}></Route>
+        <Route path='/presentation' element={<Presentation />}></Route>
+        <Route path='/grades' element={<Grades />}></Route>
         <Route path="/signup" element={<SignUp />} />
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
       {/* <Quiz /> */}
+      {/* <Profile /> */}
     </>
   );
 }
